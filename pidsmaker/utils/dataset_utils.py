b1a5ca59aac6b382b617cc0f1708c24a1d5b898b
Birvine-Dev <Birvine.dev@gmail.com>
2026-09-24 19:07:04 +1200

    Register WITFOO_84M_D8 (density ladder, frontier rung)

--- pidsmaker/utils/dataset_utils.py
+++ pidsmaker/utils/dataset_utils.py
@@ -107,7 +107,7 @@ rel2id_witfoo = {
     "USER_ACTION": 17,
 }
 
-witfoo_datasets = ["witfoo_toy", "witfoo_2m", "witfoo_2m_v2", "witfoo_2m_v2e", "witfoo_2m_v2e_s4", "witfoo_84m", "witfoo_114m"]
+witfoo_datasets = ["witfoo_toy", "witfoo_2m", "witfoo_2m_v2", "witfoo_2m_v2e", "witfoo_2m_v2e_s4", "witfoo_84m", "witfoo_84m_d8", "witfoo_114m"]
 
 rel2id_atlasv2 = {
     0: "ACTION_FILE_UNDELETE",
